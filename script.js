de7e520d3035ae3de7f9db7423085702dffa99a6
Elliot <elliotmrgn@gmail.com>
2020-03-23 20:50:43 -0400

    working on user input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var userSearch = $(".user-search");
+var userStartDate = $("user-start-date")
+var userStartDate = $("user-end-date")
+
 var input = "corona virus";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + input + "&api-key=HWlDcliekRAsANCpbWLTGjfohdoE7lpJ"
 
@@ -8,10 +12,13 @@ function getArticle() {
     method: "GET"
   }).then(function (response) {
     console.log(response);
-    var articleHead = $("<a>")
-    articleHead.attr("href", response.response.docs[0].web_url);
-    articleHead.text(response.response.docs[0].abstract);
-    $(".results").append(articleHead);
+    for (var i = 0; i < 10; i++) {
+      var articleHead = $("<a>")
+      articleHead.attr("href", response.response.docs[i].web_url);
+      articleHead.html(response.response.docs[i].headline.main + '</br>Date: ' + response.response.docs[0].pub_date);
+      articleHead.append('</br></br>')
+      $(".results").append(articleHead);
+    }
   });
 }
 
